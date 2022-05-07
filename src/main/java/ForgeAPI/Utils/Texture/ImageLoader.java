49e1915b6298789f38ead001214cf5abdfbd9953
mc23 <2963456487@qq.com>
2022-05-07 20:39:20 +0800

    1

--- src/main/java/ForgeAPI/Utils/Texture/ImageLoader.java
+++ src/main/java/ForgeAPI/Utils/Texture/ImageLoader.java
@@ -59,18 +59,32 @@ public class ImageLoader extends AbstractTexture {
         return texture.getGlTextureId();
     }
 
+    /**
+     * 获取图片的textureID
+     * @param image BufferedImage类型的图片文件
+     * */
     public static int loadTexture(BufferedImage image) throws IOException {
         ITextureObject texture = new ImageLoader(image);
         texture.loadTexture(Minecraft.getMinecraft().getResourceManager());
         return texture.getGlTextureId();
     }
 
+
+    /**
+     * 获取图片的textureID
+     * @param url 图片的网络url链接
+     * */
     public static int loadTexture(String url) throws IOException {
         ITextureObject texture = new ImageLoader(url);
         texture.loadTexture(Minecraft.getMinecraft().getResourceManager());
         return texture.getGlTextureId();
     }
 
+    /**
+     * 删除textureId
+     * 说明：删除textureId可以减少内存浪费，
+     *      建议在onGuiClosed事件中调用，以便释放内存
+     * */
     public static void deleteTexture(int textureId) {
         TextureUtil.deleteTexture(textureId);
     }
